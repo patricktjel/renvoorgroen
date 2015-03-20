3fdcfc22226ac84e96e4dcba012c5112d2a293cc
Robbin Voortman <robbinvoortman@gmail.com>
2015-03-20 21:05:43 +0100

    deleted some stuff;

--- RestVoorGroen/WebContent/web/js/addUser.js
+++ RestVoorGroen/WebContent/web/js/addUser.js
@@ -64,8 +64,6 @@ function addUser() {
             })
             .fail(function (jqXHR, textStatus) {
               console.log(jqXHR);
-              console.log(data);
-              bootbox.alert("<h1>Error making a new user.</h1>" + jqXHR.statusText);
             });
         }
       }
@@ -76,10 +74,9 @@ function addUser() {
 
 $(document).ready(function () {
   $('#table-users tbody tr').remove();
+ 
   $.ajax({
     type: 'GET',
-    contentType: "application/json",
-    dataType: "json",
     url: "http://grolschbak.cloudapp.net:8080/Restvoorgroen/api/user/get",
   })
     .done(function (data) {
@@ -88,9 +85,9 @@ $(document).ready(function () {
       data.forEach(function (user) {
         tableRow += '<tr>' +
           '<td>' + user.id + '</td>' +
-          '<td>' + user.name + '</td>' +
-          '<td>' + user.adminLevel + '</td>' +
-          '<td><button data-userId="' + user.id + ' type="button" class="btn btn-success btn-sm pull-right delete-user-btn">Remove</button> </td>' +
+          '<td>' + user.inlognaam + '</td>' +
+          '<td>' + user.naam + '</td>' +
+          '<td><button data-userId="' + user.id + ' type="button" class="btn btn-success btn-sm pull-right delete-user-btn">Delete</button> </td>' +
           '</tr>';
         $('#table-users tbody').append(tableRow);
       });
