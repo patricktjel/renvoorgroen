d6a6ce7a254c19448bffa2f7e4031823cf174f50
Robbin Voortman <robbinvoortman@gmail.com>
2015-03-20 22:46:28 +0100

    fixed typo

--- RestVoorGroen/WebContent/js/addSponser.js
+++ RestVoorGroen/WebContent/js/addSponser.js
@@ -1,6 +1,6 @@
 function addSponsor() {
   bootbox.dialog({
-    title: "Add Milestone",
+    title: "Add Sponsor",
     message: '<div class="row">  ' +
       '<div class="col-md-12"> ' +
       '<form class="form-horizontal"> ' +
