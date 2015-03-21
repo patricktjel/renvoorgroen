301f2cc559a7c4717d2a582ef15d2190db11f1dc
patricktjel <patrick.thijssen@chello.nl>
2015-03-21 08:48:46 +0100

    limit change

--- RestVoorGroen/src/resources/_Best.java
+++ RestVoorGroen/src/resources/_Best.java
@@ -3,10 +3,12 @@ package resources;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -21,17 +23,23 @@ public class _Best {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/floors")
-	public BestUser getBestFloorsUser() {
+	public ArrayList<BestUser> getBestFloorsUser(@QueryParam("limit") int limit) {
+		ArrayList<BestUser> result = new ArrayList<BestUser>();
 		try {
-			String sql = "SELECT * FROM `user_activity` WHERE activity_id=? ORDER BY `value` DESC LIMIT 1";
+			String sql = "SELECT * FROM `user_activity` WHERE activity_id=? ORDER BY `value` DESC LIMIT ?";
 			PreparedStatement stat = DatabaseHelper.getConnection().prepareStatement(sql);
 			stat.setInt(1, 1);
+			stat.setInt(2, limit);
 			ResultSet set = stat.executeQuery();
-			set.next();
-			Double value = set.getDouble("value");
-			int users_id = set.getInt("users_id");
-			User user = getuserbyId(users_id);
-			return new BestUser(user, value);
+			
+			while(set.next()){
+				Double value = set.getDouble("value");
+				int users_id = set.getInt("users_id");
+				User user = getuserbyId(users_id);
+				result.add(new BestUser(user, value));
+			}
+			
+			return result;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -42,17 +50,23 @@ public class _Best {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/steps")
-	public BestUser getBestStepsUser() {
+	public ArrayList<BestUser> getBestStepsUser(@QueryParam("limit") int limit) {
+		ArrayList<BestUser> result = new ArrayList<BestUser>();
 		try {
-			String sql = "SELECT * FROM `user_activity` WHERE activity_id=? ORDER BY `value` DESC LIMIT 1";
+			String sql = "SELECT * FROM `user_activity` WHERE activity_id=? ORDER BY `value` DESC LIMIT ?";
 			PreparedStatement stat = DatabaseHelper.getConnection().prepareStatement(sql);
 			stat.setInt(1, 2);
+			stat.setInt(2, limit);
 			ResultSet set = stat.executeQuery();
-			set.next();
-			Double value = set.getDouble("value");
-			int users_id = set.getInt("users_id");
-			User user = getuserbyId(users_id);
-			return new BestUser(user, value);
+			
+			while(set.next()){
+				Double value = set.getDouble("value");
+				int users_id = set.getInt("users_id");
+				User user = getuserbyId(users_id);
+				result.add(new BestUser(user, value));
+			}
+			
+			return result;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,17 +77,23 @@ public class _Best {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/distance")
-	public BestUser getBestDistanceUser() {
+	public ArrayList<BestUser> getBestDistanceUser(@QueryParam("limit") int limit) {
+		ArrayList<BestUser> result = new ArrayList<BestUser>();
 		try {
-			String sql = "SELECT * FROM `user_activity` WHERE activity_id=? ORDER BY `value` DESC LIMIT 1";
+			String sql = "SELECT * FROM `user_activity` WHERE activity_id=? ORDER BY `value` DESC LIMIT ?";
 			PreparedStatement stat = DatabaseHelper.getConnection().prepareStatement(sql);
 			stat.setInt(1, 3);
+			stat.setInt(2, limit);
 			ResultSet set = stat.executeQuery();
-			set.next();
-			Double value = set.getDouble("value");
-			int users_id = set.getInt("users_id");
-			User user = getuserbyId(users_id);
-			return new BestUser(user, value);
+			
+			while(set.next()){
+				Double value = set.getDouble("value");
+				int users_id = set.getInt("users_id");
+				User user = getuserbyId(users_id);
+				result.add(new BestUser(user, value));
+			}
+			
+			return result;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
